4712ee882e79b7352f7f40e04985ac84a9fb9ce9
BrunoUemura <bruno.h.uemura@outlook.com>
2021-04-22 19:00:53 -0300

    Removed dist directory

--- dist/src/database/index.js
+++
@@ -1,3 +0,0 @@
-var dbEngine = process.env.DB_ENVIRONMENT || "development";
-var config = require("./knexfile")[dbEngine];
-module.exports = require("knex")(config);
--- dist/src/models/Users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Users = /** @class */ (function () {
-    function Users() {
-    }
-    return Users;
-}());
-exports.default = Users;
