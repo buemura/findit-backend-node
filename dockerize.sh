a997fc9b1c8657b81ae90f6e0b3671976d538973
BrunoUemura <bruno.h.uemura@outlook.com>
2021-07-06 02:10:02 -0300

    Adjust in dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo -e "\n${green}[==== Building Docker Image ====]${reset}"
 docker-compose build
 docker-compose up -d
 
-sleep 30
+echo -e "\n${green}[==== Checking database relations ====]${reset}"
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost/api/users)
 
-docker exec node-1 npx sequelize-cli db:migrate
-docker exec node-1 npx sequelize-cli db:seed:all
+if [[ $status_code -ne 200 ]] ; then
+  echo -e "${green}[======= Executing  Migrations =======]${reset}"
+  sleep 30
+  docker exec node-1 npx sequelize-cli db:migrate
+  docker exec node-1 npx sequelize-cli db:seed:all
+else
+  echo -e "${green}[==== Migrations already executed ====]${reset}"
+  exit 0
+fi
