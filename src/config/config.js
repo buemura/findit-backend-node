718efe16b336131e1b9ca38752ff372021e37bc6
BrunoUemura <bruno.h.uemura@outlook.com>
2021-08-14 00:47:06 -0300

    Changing ssl config

--- src/config/config.js
+++ src/config/config.js
@@ -22,6 +22,6 @@ module.exports = {
     database: process.env.DEFAULT_DB,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    ssl: { rejectUnauthorized: false },
+    ssl: true,
   },
 };
