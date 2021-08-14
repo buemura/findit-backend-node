1f78ac375fd01ebe561605c6e9aa34467f7d5be5
BrunoUemura <bruno.h.uemura@outlook.com>
2021-08-14 00:56:19 -0300

    Changing config.js

--- src/config/config.js
+++ src/config/config.js
@@ -17,11 +17,6 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DEFAULT_DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    ssl: true,
+    use_env_variable: process.env.DATABASE_URL,
   },
 };
